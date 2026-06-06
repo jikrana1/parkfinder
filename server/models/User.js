f20e3fabbd1fbe864ac4931af34a6632745771ac
Mohammad-Hassan027 <shaikhmohammadhassan027@gmail.com>
2026-06-06 16:40:42 +0530

    feat: add favorites array to user schema (#51)

--- server/models/User.js
+++ server/models/User.js
@@ -8,12 +8,17 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: "user" },
   resetToken: String,
   resetTokenExpiry: Date,
+  favorites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Parking",
+    },
+  ],
 });
 
 // Password hashing
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-
   this.password = await bcrypt.hash(this.password, 10);
 });
 
